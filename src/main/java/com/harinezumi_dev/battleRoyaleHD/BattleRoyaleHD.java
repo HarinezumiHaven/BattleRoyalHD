4b93d7490f4051d09d28afb600c94301f7de2f91
HarinezumiHaven <matvio4545@gmail.com>
2026-02-03 14:25:09 +0100

    Project setup complete

---
+++ src/main/java/com/harinezumi_dev/battleRoyaleHD/BattleRoyaleHD.java
@@ -0,0 +1,17 @@
+package com.harinezumi_dev.battleRoyaleHD;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BattleRoyaleHD extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
